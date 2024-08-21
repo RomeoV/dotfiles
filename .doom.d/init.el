a064d3dd2ae9d814889d617062be69966ece5828
Romeo Valentin <romeov@stanford.edu>
2024-08-21 10:13:55 +0200

    update emacs dotfiles.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -4,8 +4,8 @@
 ;; in. Remember to run 'doom sync' after modifying it!
 
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find a "Module Index" link where you'll find
-;;      a comprehensive list of Doom's modules and what flags they support.
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
 
 ;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
 ;;      'C-c c k' for non-vim users) to view its documentation. This works on
@@ -15,12 +15,14 @@
 ;;      directory (for easy access to its source code).
 
 (doom! :input
+       ;;bidi              ; (tfel ot) thgir etirw uoy gnipleh
        ;;chinese
        ;;japanese
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       ;; (company +childframe)           ; the ultimate code completion backend
+       (corfu +orderless +icons)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
@@ -34,17 +36,17 @@
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       ;; indent-guides     ; highlighted indent columns
-       ;; ligatures         ; ligatures and symbols to make your code pretty again
+       indent-guides     ; highlighted indent columns
+       ;;ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       ;; nav-flash         ; blink cursor line after big motions
+       nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
-       ;; ophints           ; highlight the region an operation acts on
+       ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
-       unicode           ; extended unicode support for various languages
+       ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty)         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
@@ -55,9 +57,9 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       format            ; automated prettiness
+       (format +onsave)            ; automated prettiness
        ;;god             ; run Emacs commands without modifier keys
-       ;; lispy             ; vim for lisp, for people who don't like vim
+       ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
@@ -79,42 +81,42 @@
        vterm             ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax +childframe) ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
+       syntax ; tasing you for every semicolon you forget
+       (spell +hunspell +flyspell) ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
        biblio            ; Writes a PhD for you (citation needed)
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       collab            ; buffers with friends
+       (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        direnv
        ;;docker
        editorconfig      ; let someone else argue about tabs vs spaces
        ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
        (lookup +dictionary +docsets)              ; navigate your code and its documentation
        (lsp +eglot)               ; M-x vscode
        (magit +forge)             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       (pdf +pdf-tools)               ; pdf enhancements
+       pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       ;; tree-sitter       ; syntax and parsing, sitting in a tree...
+       tree-sitter       ; syntax and parsing, sitting in a tree...
        upload
                                         ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
-       ;;beancount         ; mind the GAAP
+       (beancount +lsp)         ; mind the GAAP
        ;;cc                ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
@@ -143,34 +145,37 @@
        ;;json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       (julia +tree-sitter +lsp)               ; a better, faster MATLAB
+       (julia +snail +lsp +tree-sitter)               ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +latexmk +cdlatex +fold)             ; writing papers in Emacs has never been so fun
-       ;;lean              ; for folks with too much to prove
+       (latex +latexmk +cdlatex +fold +lsp)             ; writing papers in Emacs has never been so fun
+       lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
        (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       (nix)               ; I hereby declare "nix geht mehr!"
+       (nix +lsp +tree-sitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
+        +gnuplot
         +hugo
         +dragndrop
         +roam2
         +present
         +pomodoro
-        +pandoc)           ; organize your plain life in plain text
+        +pandoc
+        ;; +pretty
+        )           ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +pyright +tree-sitter +poetry)            ; beautiful is better than ugly
+       (python +poetry +lsp +tree-sitter)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
@@ -196,5 +201,5 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ; literate
+       literate
        (default +bindings +smartparens))
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -10,7 +10,7 @@
 ;; (package! with-editor :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab")
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+                                        ;(package! some-package)
 (package! org-inline-pdf)
 
 ;; (package! org-ref)
@@ -20,92 +20,145 @@
 ;; (package! ivy-bibtex)
 (package! org-attach-screenshot)
 
+(package! dirvish)
+
+(package! agenix
+  ;; :recipe (:host github :repo "RomeoV/agenix.el"))
+  :recipe (:local-repo "/home/romeo/Documents/github/agenix.el"))
+
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+;; (package! copilot
+;;   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+;; (package! copilot
+;;   :recipe (:host github :repo "RomeoV/emacs-copilot" :branch "enable-ssh))
+;; (package! org-tufte
+;;   :recipe (:host github :repo "Zilong-Li/org-tufte"))
+(package! org-tufte
+  :recipe (:local-repo "/home/romeo/Documents/github/org-tufte"))
+;; (package! ox-tufte)
+
+;;(package! difftastic
+;;  :recipe (:host github :repo pkryger/difftastic.el"))
+
+(package! gptel)
+(package! modus-themes)
+
+;; (package! eat)
+
 ;; (package! julia-mode)
 ;; (package! julia-vterm)
 ;; (package! ob-julia-vterm)
 ;; (package! tree-sitter-langs)
 ;; (package! tree-sitter-julia
 ;;   :recipe (:host github :repo "tree-sitter/tree-sitter-julia"))
-(package! julia-ts-mode)
-(package! evil-textobj-tree-sitter)
+
+;; (package! julia-ts-mode)
+
+;; (package! evil-textobj-tree-sitter)
+;; (package! evil-textobj-tree-sitter
+;;   :recipe (:host github
+;;            :repo "meain/evil-textobj-tree-sitter"
+;;            :files (:defaults "queries" "treesit-queries")
+;;            :branch "treesit"))
+;; (package! evil-ts :recipe (:host github :repo "foxfriday/evil-ts"))
 ;; (package! combombulate
 ;;   :recipe (:host github :repo "mickeynp/combobulate"))
 ;; (package! eglot-jl :ignore t)
 ;;
+;; From https://discord.com/channels/406534637242810369/1078228006176301087/1078228432057536562
+;; (package! eglot :built-in 'prefer)
+;; (package! treesit :built-in 'prefer)
+
+;; (package! ts-movement
+;;   :recipe (:host github :repo "haritkapadia/ts-movement"))
 
 ;; (package! nano-emacs
 ;;   :recipe (:host github :repo "rougier/nano-emacs"))
 ;;
-; (package! magit :pin "f44f6c14500476d918e9c01de8449edb20af4113")
-; (package! forge :pin "ecedeaf641f3c06ac72db57837d15bdb02ac198b")
+                                        ; (package! magit :pin "f44f6c14500476d918e9c01de8449edb20af4113")
+                                        ; (package! forge :pin "ecedeaf641f3c06ac72db57837d15bdb02ac198b")
 
+(package! eglot-booster
+  :recipe (:host github :repo "jdtsmith/eglot-booster"))
 
 (package! engrave-faces)
 ;; (package! ob-julia :recipe (:local-repo "lisp/ob-julia" :files ("*.el" "julia")))
-(package! magit-delta)
+                                        ; (package! magit-delta)
 
-(package! org-caldav)
+(package! git-link)
 
-; (package! nickel-mode)
-(package! org-appear)
+(package! typst-ts-mode
+  :recipe (:type git :host sourcehut :repo "meow_king/typst-ts-mode" :files (:defaults "*.el")))
+
+
+;; (package! org-caldav)
+
+                                        ; (package! nickel-mode)
+                                        ; (package! org-appear)
 
 ;; There's some new bug related to org-loaddefs...
 (package! ox-latex-subfigure
- :recipe (:host github :repo "RomeoV/ox-latex-subfigure"))
+  :recipe (:host github :repo "RomeoV/ox-latex-subfigure"))
 ;; (package!  powerthesaurus
 ;;   :recipe (:host github :repo "SavchenkoValeriy/emacs-powerthesaurus"))
 ;; (package!  define-word
 ;;   :recipe (:host github :repo "abo-abo/define-word"))
+                                        ; (package!  symex
+                                        ;   :recipe (:host github :repo "drym-org/symex.el" :branch 2.0-integration))
+;; (package! combobulate)
 
 ;; (package! straight :pin "3eca39d")  ;; see https://github.com/doomemacs/doomemacs/issues/6960#issuecomment-1327514660
 
 ;; (package! direnv)
 (package! isend-mode)                   ;; <- this is the real game changer :)
-; (package! org-transclusion)
+                                        ; (package! org-transclusion)
 
-; (package! crdt)
+                                        ; (package! crdt)
 (package! org-ai)
 
 
-;; (package! org-modern)
+(package! org-modern)
+(package! org-modern-indent
+  :recipe (:host github :repo "jdtsmith/org-modern-indent"))
 ;; (package! openwith)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
+                                        ;(package! another-package
+                                        ;  :recipe (:host github :repo "username/repo"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
+                                        ;(package! this-package
+                                        ;  :recipe (:host github :repo "username/repo"
+                                        ;           :files ("some-file.el" "src/lisp/*.el")))
 
 ;; If you'd like to disable a package included with Doom, you can do so here
 ;; with the `:disable' property:
-;(package! builtin-package :disable t)
+                                        ;(package! builtin-package :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
 ;; all the properties for `:recipe'. These will inherit the rest of its recipe
 ;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
+                                        ;(package! builtin-package :recipe (:nonrecursive t))
+                                        ;(package! builtin-package-2 :recipe (:repo "myfork/package"))
 
 ;; Specify a `:branch' to install a package from a particular branch or tag.
 ;; This is required for some packages whose default branch isn't 'master' (which
 ;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
+                                        ;(package! builtin-package :recipe (:branch "develop"))
 
 ;; Use `:pin' to specify a particular commit to install.
-;(package! builtin-package :pin "1a2b3c4d5e")
+                                        ;(package! builtin-package :pin "1a2b3c4d5e")
 
 
 ;; Doom's packages are pinned to a specific commit and updated from release to
 ;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
+                                        ;(unpin! pinned-package)
 ;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
+                                        ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
+                                        ;(unpin! t)
